e46cd6ce27d46335b6df6fdc6dac8b4ed4e0322f
0xtheM7 <openmyaddress@gmail.com>
2025-04-10 16:36:09 +0545

    Updated background

---
+++ hency.js
@@ -0,0 +1,23 @@
+function search() {
+  const searchEngine = document.getElementById("searchEngine").value;
+  const query = document.getElementById("query").value;
+  if (query.trim()=== "") return;
+  window.open(searchEngine + encodeURIComponent(query),"_self");
+  
+}
+
+// EventListener to capture the event of the button 
+
+// when user clicks the button 
+document.getElementById('searchButton').addEventListener('click',search);
+
+
+// when user clciks enter 
+
+document.getElementById("query").addEventListener("keypress",
+  function(e){
+    if(e.key === "Enter"){
+      search();
+    }
+  }
+)
